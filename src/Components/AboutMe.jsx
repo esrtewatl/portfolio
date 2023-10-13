b537fe34dc130f8b3bcd44a8467cb3ecf598ea11
esrtewatl <estewart1979@gmail.com>
2023-10-13 19:16:54 -0400

    changes

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,10 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import "./AboutMe.css"; // Import custom CSS styles for the About Me section
 import Quotes from "./Quotes";
 
-
-// import TypeAnimate from "./TypeAnimate";
 const AboutMe = () => {
   const sectionVariants = {
     hidden: { opacity: 0, y: 50 },
@@ -23,49 +21,62 @@ const AboutMe = () => {
       transition: {
         type: "spring", // Apply a spring animation
         stiffness: 100, // Adjust stiffness for the bounce effect
-        damping: 10,    // Adjust damping for the bounce effect
+        damping: 10, // Adjust damping for the bounce effect
         duration: 1.5,
-        delay: 1,       // Delay the animation
+        delay: 1, // Delay the animation
       },
     },
   };
 
+  const [isSummaryInView, setIsSummaryInView] = useState(false);
+  const summaryRef = useRef();
+
+  const handleIntersection = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setIsSummaryInView(true);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const options = {
+      threshold: 0.5, // Adjust this threshold as needed
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    if (summaryRef.current) {
+      observer.observe(summaryRef.current);
+    }
+
+    // Clean up the observer when the component unmounts
+    return () => {
+      if (summaryRef.current) {
+        observer.unobserve(summaryRef.current);
+      }
+    };
+  }, []);
 
   return (
     <motion.section
-
       className="about-me-section"
       variants={sectionVariants}
       initial="hidden"
       animate="visible"
     >
-     {/* <TypeAnimate /> */}
-        <h2>Software Developer</h2>  
-         <motion.h1
-        className="name"
-        variants={nameVariants}
-        initial="hidden"
-        animate="visible"
-     
-      >
- 
+      <h2>Software Developer</h2>
+      <motion.h1 className="name" variants={nameVariants} initial="hidden" animate="visible">
         Erica Stewart
       </motion.h1>
-   <Quotes />
-<div className="summary">
-
-      <motion.p className='summaryP' variants={itemVariants}>
-     
-      Music sparked my web design journey, leading me to 5 years of dedicated experience as a Frontend React Developer. My expertise thrives in crafting engaging web interfaces with animation and sophisticated UX/UI  design, ensuring fantastic user experiences. Proficient in JavaScript, CSS3, and HTML5, I embrace diversity in browser behaviors and prioritize accessibility and design standards. Figma,  Framer, BootStrap and Chakra Ui are my tools of choice, and I thrive on writing clear, maintainable code while contributing to all aspects of the development process.
-      </motion.p>
+      <Quotes />
+      <div className="summary" ref={summaryRef}>
+        {isSummaryInView && (
+          <motion.p className="summaryP" variants={itemVariants}>
+        My passion for web design setting me on a five-year journey as a dedicated Frontend React Developer. I specialize in creating engaging web interfaces with a focus on animation and refined UX/UI design to provide exceptional user experiences. Proficient in JavaScript, CSS3, and HTML5, I prioritize compatibility across browsers and adhere to accessibility and design standards. My toolkit includes industry-standard tools such as Figma, Framer, Bootstrap, and Chakra UI, enabling me to craft clean, maintainable code and contribute comprehensively to the development process.
+          </motion.p>
+        )}
       </div>
-  
-
-  
-     
-     
-   
-
     </motion.section>
   );
 };
